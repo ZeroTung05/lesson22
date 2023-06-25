94cac5f67bcaabb80a4004274ea578f403972dfe
ZeroTung05 <s.ch2605@mail.ru>
2023-06-25 20:07:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        final int X= 10;
+        System.out.println(X);
+    }
+}
+class Test{
+    public static final int CONSTANT= 0;
+
+
+
+}
